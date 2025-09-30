52237bc9352fdb0cdbe88d41088529377414fa02
rev3nt <mikhail.merkulov.2005@mail.ru>
2025-10-01 01:26:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import math
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        return
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
+
+
+circle = Circle(2)
+
+print(circle.area())
+
+rectangle = Rectangle(10, 2)
+
+print(rectangle.area())
